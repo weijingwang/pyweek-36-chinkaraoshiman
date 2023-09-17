1f4ca72ae84cbffdc6d3f0577f18ece9ca2a0b9e
Jiahua Ren <jiahuaren135@gmail.com>
2023-09-17 15:40:46 -0700

    set up basic pygame screen/game

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pygame
+
 #main game is platformer
 #player jumps and stands on walls
 #player can shoot gun really fast
@@ -5,3 +7,19 @@
 #bullets
 #player can talk to npcs
 #player moves to room/change level by going though a door
+
+pygame.init()
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption("TEST")
+
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+pygame.quit()
