dcb8557ce36bfb703a0cdbef917514254031f463
Weijing Wang <weijingrocks@gmail.com>
2023-09-17 15:45:03 -0700

    tutorial tesrting

---
+++ game.py
@@ -0,0 +1,63 @@
+import sys
+
+import pygame
+
+from scripts.utils import load_image, load_images
+from scripts.entities import PhysicsEntity
+from scripts.tilemap import Tilemap
+
+class Game:
+    def __init__(self):
+        pygame.init()
+
+        pygame.display.set_caption('pyweek36')
+        self.screen = pygame.display.set_mode((1280, 720))
+        self.display = pygame.Surface((320, 240))
+
+        self.clock = pygame.time.Clock()
+        
+        self.movement = [False, False]
+        
+        self.assets = {
+            # 'decor': load_images('tiles/decor'),
+            'grass': load_images(''),
+            # 'large_decor': load_images('tiles/large_decor'),
+            'stone': load_images(''),
+            'player': load_image('player.png')
+        }
+        
+        self.player = PhysicsEntity(self, 'player', (50, 50), (8, 15))
+        
+        self.tilemap = Tilemap(self, tile_size=16)
+        
+    def run(self):
+        while True:
+            self.display.fill((14, 219, 248))
+            
+            self.tilemap.render(self.display)
+            
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display)
+            
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = True
+                    if event.key == pygame.K_UP:
+                        self.player.velocity[1] = -3
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_LEFT:
+                        self.movement[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.movement[1] = False
+            
+            self.screen.blit(pygame.transform.scale(self.display, self.screen.get_size()), (0, 0))
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
--- main.py
+++ main.py
@@ -5,3 +5,28 @@
 #bullets
 #player can talk to npcs
 #player moves to room/change level by going though a door
+
+import sys
+
+import pygame
+
+class Game:
+    def __init__(self):
+        pygame.init()
+        
+        pygame.display.set_caption('pyweek 36')
+        self.screen = pygame.display.set_mode((1280, 720))
+
+        self.clock = pygame.time.Clock()
+        
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    pygame.quit()
+                    sys.exit()
+            
+            pygame.display.update()
+            self.clock.tick(60)
+
+Game().run()
