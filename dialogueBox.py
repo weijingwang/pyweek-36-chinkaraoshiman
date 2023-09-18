3c22d863478d370b6eb90d9fba64a85bf7c3fae9
ChickPhilA <51455470+ChickPhilA@users.noreply.github.com>
2023-09-17 23:24:12 -0700

    Update dialogueBox.py

--- dialogueBox.py
+++ dialogueBox.py
@@ -3,3 +3,10 @@ import pygame
 #diologe box near bottom of screen like visual novel
 #show next text after press space
 #despawn when diologue is finished
+
+class dialoguePanel:
+  def __init__(self):
+    pygame.init()
+    self.width = 250
+    self.height = 215
+    self.screen = pygame.display.set_mode((self.width, self.height))
