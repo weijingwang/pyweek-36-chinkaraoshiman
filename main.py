85091751325f32a73de0a1a0fb599132a83f523b
Weijing Wang <weijingrocks@gmail.com>
2023-09-18 16:32:32 -0700

    reorganize file structure -wj

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import pygame
+from scripts import utils
 
 #main game is platformer
 #player jumps and stands on walls
@@ -8,27 +8,3 @@ import pygame
 #player can talk to npcs
 #player moves to room/change level by going though a door
 
-pygame.init()
-
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption("TEST")
-
-
-x = 200
-y = 200
-scale = 3
-# img = pygame.image.load(PATH TO IMG)
-# img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
-# rect = img.get_rect()
-# rect.center = (x,y)
-
-run = True
-while run:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            run = False
-
-pygame.quit()
