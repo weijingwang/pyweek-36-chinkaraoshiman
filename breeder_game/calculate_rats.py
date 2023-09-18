3e4e386caa394da22ceecc30c5c5c951b6523cca
Weijing Wang <weijingrocks@gmail.com>
2023-09-17 23:54:39 -0700

    rat spawn algorithm - weijing

---
+++ breeder_game/calculate_rats.py
@@ -0,0 +1,37 @@
+#every second the rat status will update according to this code
+
+#real time graphing????
+
+rat_count = 0
+upper_cap = 0
+
+
+#every turn, healhy status decreases if no food or bad conditions
+#flucuate randomly
+
+if rat_count == 0:
+    pass
+elif rat_count == 1:
+    pass
+    #get eaten?
+    #reach uppercap?
+elif rat_count < 1:
+    pass
+    #get eaten?
+    #exponential growth
+    #reach uppercap?
+
+if rat_count == upper_cap:
+    rat_count = upper_cap
+elif rat_count > upper_cap:
+    #decay rat count
+    pass
+
+
+#if unhealthy guaranteed mice die.
+# if 0% healthy 50% die
+# else, die proportional to health
+
+#crow can eat mice.
+# chance that crow comes
+#if 1 crow comes, every second, 1 mice dies
