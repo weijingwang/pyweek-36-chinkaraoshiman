28cb70f95ee9ef6ebe3614abb8148715671a4ebf
Jiahua Ren <jiahuaren135@gmail.com>
2023-09-17 20:29:13 -0700

    add superclass Mob and extend to it from Enemy

--- main.py
+++ main.py
@@ -16,6 +16,15 @@ SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("TEST")
 
+
+x = 200
+y = 200
+scale = 3
+# img = pygame.image.load(PATH TO IMG)
+# img = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+# rect = img.get_rect()
+# rect.center = (x,y)
+
 run = True
 while run:
     for event in pygame.event.get():
