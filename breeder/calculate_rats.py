cbc6026bc92765855bd4a6f80399f8374e636649
Weijing Wang <weijingrocks@gmail.com>
2023-09-19 22:37:13 -0700

    no asexual reprodcution -wj

--- breeder/calculate_rats.py
+++ breeder/calculate_rats.py
@@ -26,9 +26,10 @@ class BreederCalculations:
                 self.next_increase -= self.rat_count/3
 
     def calculate_next_change(self):
-        self.next_increase += self.rat_count / 3
-        if (self.next_increase+self.rat_count) > self.upper_cap:
-            self.next_increase = self.upper_cap - self.rat_count
+        if self.rat_count <= 1:
+            self.next_increase += self.rat_count / 3
+            if (self.next_increase+self.rat_count) > self.upper_cap:
+                self.next_increase = self.upper_cap - self.rat_count
 
     def update(self):
         if self.timer == 60:
