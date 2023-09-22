84897604b95a9fef2ad776a74c4c17f82f8ab0a4
Jiahua Ren <jiahuaren135@gmail.com>
2023-09-21 22:01:44 -0700

    add dogfood and potion assets add rat asset create Rat class create Item class

--- platformer_game.py
+++ platformer_game.py
@@ -1,6 +1,6 @@
 import sys
 
-from platformer.entities import Object, Player
+from platformer.entities import Player, Rat, Item
 from platformer.tilemap import Tilemap
 from utils import load_image
 
@@ -15,9 +15,6 @@ class Game:
         self.screen = pygame.display.set_mode((1280, 720))
         #i think this is a good resolution 3:1 ratio
         self.display = pygame.Surface((1280/3, 720/3))
-        #or maybe this one
-        #can change this for testing code
-        self.display = pygame.Surface((1280/2, 720/2))
         
         self.clock = pygame.time.Clock()
         
@@ -26,11 +23,14 @@ class Game:
             'stone': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
             'checkpoint': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
             'grass': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
+            'rat': load_image('platformer/rat.png'),
             'brick1': load_image('platformer/brick1.png'),
             'brick2': load_image('platformer/brick2.png'),
             'brick3': load_image('platformer/brick3.png'),
             'brick4': load_image('platformer/brick4.png'),
             'brick5': load_image('platformer/brick5.png'),
+            'dogfood': load_image('platformer/dogfood.png'),
+            'potion': load_image('platformer/potion.png')
         }
     
         self.tilemap = Tilemap(self, tile_size=32)
@@ -41,6 +41,11 @@ class Game:
 
         self.player = Player(self, 'player', (100, 100), (16, 30))
 
+        self.ALL_ITEMS = ['dogfood', 'potion']
+        self.rats = [Rat(self, 'rat', (100, 150), (15, 16))]
+        self.items = [Item(self, (110, 150), (32,32)), Item(self, (130, 150), (32,32))]
+
+
     def run(self):
         while True:
             self.display.fill((156, 153, 78))
@@ -49,8 +54,20 @@ class Game:
             self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 20
             camera = (int(self.scroll[0]), int(self.scroll[1]))
 
+            # define map tiles and mobs here
+            # tiles: in Tilemap
+            # rats, npcs, enemies, items: render and update here 
             self.tilemap.render(self.display, offset=camera)
+
+            for rat in self.rats:
+                rat.update()
+                rat.render(self.display, offset=camera)
             
+            for item in self.items:
+                item.update()
+                item.render(self.display, offset=camera)
+
+
             self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
             self.player.render(self.display, offset=camera)
             
