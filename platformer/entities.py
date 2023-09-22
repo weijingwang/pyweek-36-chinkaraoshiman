6520757a3bd94f836e79f51cbd914466d6cfd78f
Weijing Wang <weijingrocks@gmail.com>
2023-09-22 10:41:02 -0700

    Revert "Revert "Merge remote-tracking branch 'origin/platformer' into breedinggame""
    This reverts commit ed73801209462f719866144ce0b310be45061261.

--- platformer/entities.py
+++ platformer/entities.py
@@ -1,5 +1,7 @@
 import pygame
+import random
 from platformer.tilemap import Tilemap
+from utils import CounterText
 
 class Object:
     def __init__(self, game, etype, pos, size):
@@ -65,3 +67,112 @@ class Player(Object):
                 if self.rect().colliderect(pygame.Rect(tile['pos'][0] * tile_size - 1, tile['pos'][1] * tile_size - 1, tile_size + 2, tile_size + 2)):
                     return True
         return False
+    
+    def touching_rat(self):
+        tilemap = self.game.tilemap
+        tile_size = tilemap.tile_size
+
+        for tile in tilemap.tiles_around(self.pos):
+            if (tile['type'] == 'rat'):
+                if self.rect().colliderect(pygame.Rect(tile['pos'][0] * tile_size - 1, tile['pos'][1] * tile_size - 1, tile_size + 2, tile_size + 2)):
+                    return True
+        return False
+        
+
+"""
+Spawn items which will drop down to the ground
+(kinda hard to fine tune each item rects, so for now they will be same sizes and drop down)
+"""
+class Item(Object):
+    def __init__(self, game, pos, size):
+        item_type = random.choice(game.ALL_ITEMS)
+        self.on_ground = False
+        super().__init__(game, item_type, pos, size)
+
+    def update(self):
+        if not self.on_ground:
+            self.ground_collision = False
+
+            # only x movement should be random, y should be 0 as it doesn't jump.
+            gravity = self.vel[1]
+            
+            tilemap = self.game.tilemap
+
+            self.pos[1] += gravity
+            self_rect = self.rect()
+            for rect in tilemap.rects_around(self.pos):
+                if self_rect.colliderect(rect):
+                    self_rect.bottom = rect.top
+                    self.ground_collision = True
+                    self.pos[1] = self_rect.y
+
+            self.vel[1] = min(5, self.vel[1] + 0.1)
+
+            if self.ground_collision:
+                self.vel[1] = 0
+                self.on_ground = True
+
+    
+
+class Rat(Object):
+    def __init__(self, game, etype, pos, size):
+        """self.range = range
+        self.last_move = 0"""
+        super().__init__(game, etype, pos, size)
+
+    def update(self):
+        if self.touching_player():
+            self.game.rats.remove(self)
+        textbox = CounterText()
+        textbox.render("rat rat", self.game.display, 100, 100)
+        
+        self.collisions = {'up': False, 'down': False, 'left': False, 'right': False}
+
+        # only x movement should be random, y should be 0 as it doesn't jump.
+        movement = [0,0]
+        movement_tuple = (movement[0] + self.vel[0], movement[1] + self.vel[1])
+        
+        tilemap = self.game.tilemap
+
+        """# rat should move in a specified range, but never out that range
+        # range is how many pixels it can move in left and right direction from original pos 
+        # |---(range# of pxs)----(rat)----(range# of pxs)---|
+        # example: range is 5, if moved 4 to the left, can only move left 1 or max 5 to the right.
+        if self.last_move <= 0:
+            self.pos[0] += random.choice(range(-5, 0))
+        self.pos[0] += self.range """
+
+
+        """# check tiles around for collision
+        # use current position
+        self.pos[0] += movement_tuple[0]
+        self_rect = self.rect()
+        for rect in tilemap.rects_around(self.pos):
+            if self_rect.colliderect(rect):
+                if movement_tuple[0] > 0:
+                    self_rect.right = rect.left
+                    self.collisions['right'] = True
+                if movement_tuple[0] < 0:
+                    self_rect.left = rect.right
+                    self.collisions['left'] = True
+                self.pos[0] = self_rect.x"""
+
+        # y code is still here for gravity.
+        self.pos[1] += self.vel[1]
+        self_rect = self.rect()
+        for rect in tilemap.rects_around(self.pos):
+            if self_rect.colliderect(rect):
+                if movement_tuple[1] > 0:
+                    self_rect.bottom = rect.top
+                    self.collisions['down'] = True
+                if movement_tuple[1] < 0:
+                    self_rect.top = rect.bottom
+                    self.collisions['up'] = True
+                self.pos[1] = self_rect.y
+
+        self.vel[1] = min(5, self.vel[1] + 0.1)
+
+        if self.collisions['down'] or self.collisions['up']:
+            self.vel[1] = 0
+    def touching_player(self):
+        return self.rect().colliderect(self.game.player.rect())
--- platformer_game.py
+++ platformer_game.py
@@ -1,8 +1,9 @@
 import sys
 
-from platformer.entities import Object, Player
+from platformer.entities import Player, Rat, Item
 from platformer.tilemap import Tilemap
 from utils import load_image
+from utils import CounterText
 
 import pygame
 
@@ -15,9 +16,6 @@ class Game:
         self.screen = screen
         #i think this is a good resolution 3:1 ratio
         self.display = pygame.Surface((1280/3, 720/3))
-        #or maybe this one
-        #can change this for testing code
-        self.display = pygame.Surface((1280/2, 720/2))
         
         self.clock = pygame.time.Clock()
         
@@ -26,11 +24,16 @@ class Game:
             'stone': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
             'checkpoint': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
             'grass': pygame.transform.scale(load_image('platformer/shadow.png'), (32,32)),
+            'rat': load_image('platformer/rat.png'),
             'brick1': load_image('platformer/brick1.png'),
             'brick2': load_image('platformer/brick2.png'),
             'brick3': load_image('platformer/brick3.png'),
             'brick4': load_image('platformer/brick4.png'),
             'brick5': load_image('platformer/brick5.png'),
+            'dogfood': load_image('platformer/dogfood.png'),
+            'potion': load_image('platformer/potion.png'),
+            'tree': pygame.transform.scale(load_image('platformer/tree.png'), (64, 128)),
+            'tree2': pygame.transform.scale(load_image('platformer/tree2.png'), (64, 128))
         }
     
         self.tilemap = Tilemap(self, tile_size=32)
@@ -41,6 +44,11 @@ class Game:
 
         self.player = Player(self, 'player', (100, 100), (16, 30))
 
+        self.ALL_ITEMS = ['dogfood', 'potion']
+        self.rats = [Rat(self, 'rat', (100, 150), (15, 16))]
+        self.items = [Item(self, (110, 150), (32,32)), Item(self, (130, 150), (32,32))]
+
+
     def run(self):
         self.display.fill((156, 153, 78))
         
@@ -48,17 +56,29 @@ class Game:
         self.scroll[1] += (self.player.rect().centery - self.display.get_height() / 2 - self.scroll[1]) / 20
         camera = (int(self.scroll[0]), int(self.scroll[1]))
 
-        self.tilemap.render(self.display, offset=camera)
-        
-        self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
-        self.player.render(self.display, offset=camera)
-        
-        if self.player.touching_checkpoint():
-            # do stuff
-            # save coordinates
-            # go back to rat breeder game
-            print("touch")
-            pass
+            # define map tiles and mobs here
+            # tiles: in Tilemap
+            # rats, npcs, enemies, items: render and update here 
+            self.tilemap.render(self.display, offset=camera)
+
+            for rat in self.rats:
+                rat.update()
+                rat.render(self.display, offset=camera)
+            
+            for item in self.items:
+                item.update()
+                item.render(self.display, offset=camera)
+
+
+            self.player.update(self.tilemap, (self.movement[1] - self.movement[0], 0))
+            self.player.render(self.display, offset=camera)
+            
+            if self.player.touching_checkpoint():
+                # do stuff
+                # save coordinates
+                # go back to rat breeder game
+                print("touch")
+                pass
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
